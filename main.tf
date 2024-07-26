4e64ccfa6bf0cefb6bfd1c87b6dd9a68cb49ef1d
sarathyplkr5 <sarathy.palaykar@us.bosch.com>
2024-07-26 09:47:16 -0400

    syntax errors

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_instance" "blog" {
   instance_type = var.instance_type
 
   vpc_security_group_ids = [aws_security_group.blog.id]
-  
+
   tags = {
     Name = "HelloWorld"
   }
@@ -32,7 +32,6 @@ resource "aws_instance" "blog" {
 resource "aws_security_group" "blog" {
   name = "blog"
   description = "Allow http and https in. Allow everything out"
-
   vpc_id = data.aws_vpc.default.id
 }
 
@@ -41,8 +40,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
-  cidr_blocks = [0.0.0.0/0"]
-
+  cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
 }
 
@@ -51,8 +49,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   from_port   = 443
   to_port     = 443
   protocol    = "tcp"
-  cidr_blocks = [0.0.0.0/0"]
-
+  cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
 }
 
@@ -61,7 +58,6 @@ resource "aws_security_group_rule" "blog_everything_out" {
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
-  cidr_blocks = [0.0.0.0/0"]
-
+  cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
 }
