ffaf4d64b519955860bc7e9d5f2a3980af5fb9f9
sarathyplkr5 <sarathy.palaykar@us.bosch.com>
2024-08-07 19:11:47 -0400

    asdsad

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ module "blog_sg" {
   source = "terraform-aws-modules/security-group/aws"
 
   name        = "blog"
-  vpc_id      = module.blog_vpc.id
+  vpc_id      = module.blog_vpc.vpc_id
 
   ingress_cidr_blocks      = ["0.0.0.0/0"]
   ingress_rules            = ["https-443-tcp", "http-80-tcp"]
